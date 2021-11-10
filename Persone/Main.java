5ce5783f18f887366629e74d69a4226bd40a7861
Fraxxs <cesarettistefano1@gmail.com>
2021-11-10 17:38:17 +0100

    aggiunti i commenti

--- Persone/Main.java
+++ Persone/Main.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static Persona p;
+    public static Persona p; //dichiarata una variabile di tipo "Persona" che verrà
+                             //usata nella riga 36.
 
-    public static void chiSei()
-    {
+    public static void chiSei() //funzione che stampa a video gli elementi
+    {                           //inseriti nella classe "Persona"
         System.out.println("Sono una persona di nome: " + p.getNome() + ", sesso: " + p.getSesso() + ", età: " + p.getEtà() + ", professione: " + p.getProfessione() + ".");
     }
     public static void main(String[]args)
     {
         Scanner input = new Scanner(System.in);
 
-        String nome = "";
+        String nome = ""; //inizializate delle variabili usate nella riga 36.
         String sesso = "";
         int età = 0;
         String professione = "";
 
-        System.out.print("Inserisci il nome: ");
+        System.out.print("Inserisci il nome: "); //serie di input delle variabili precedenti
         nome = input.next();
 
         System.out.print("Inserisci il sesso: ");
@@ -34,7 +35,7 @@ public class Main
 
         p = new Persona (nome, sesso, età, professione);
 
-        chiSei();
+        chiSei(); //richiamo al metodo che stampa la classe "Persona".
 
         input.close();
     }    
