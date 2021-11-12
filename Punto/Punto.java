be556f1ba5b56449e03d50df714b30ab5abbb03e
Fraxxs <cesarettistefano1@gmail.com>
2021-11-12 17:29:25 +0100

    Creata la classe Punto
    contiene tutti i metodi e i costruttori usati nel Main

---
+++ Punto/Punto.java
@@ -0,0 +1,57 @@
+package Punto;
+
+public class Punto
+{
+    private double x;
+    private double y;
+
+    public Punto(double x, double y)
+    {
+        setX(x);
+        setY(y);
+    }
+
+    public Punto(Punto p)
+    {
+        x = p.getX();
+        y = p.getY();
+    }
+
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public double distanza(Punto p)
+    {
+        double dx = x - p.getX();
+        double dy = y - p.getY();
+        return Math.sqrt((dx * dx) + (dy * dy));
+    }
+
+    public boolean equals(Punto p)
+    {
+        return ((x == p.x) && (y == p.y));
+    }
+
+    public String toString()
+    {
+        String s = "(" + x + ";" + y + ")";
+        return s;
+    }
+}
