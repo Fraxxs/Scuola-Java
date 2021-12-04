ee339f021aad5d994ba14522dde5f88d1a33c0be
Fraxxs <cesarettistefano1@gmail.com>
2021-12-04 10:52:38 +0100

    Classe che genera un alfanumerico casuale
    Ho fatto in modo che l'alfanumerico della scuola fosse casuale e qui spiego nei dettagli come si genera.

--- Scuole/AlfanumericoRandomico.java
+++ Scuole/AlfanumericoRandomico.java
@@ -1,6 +1,4 @@
-//allora questo è un codice che genera un alfanumerico casuale, io l'ho fatto ma non ho capito una sega
-//di alcune cose che ho usato, ho cercato di informarmi il più possibile ma ancora niente di chiaro
-//appena capisco qualcosa aggiungo più commenti
+//codice che genera randomicamente un codice alfanumerico
 package Scuole;
 
 import java.util.Random;
@@ -25,16 +23,34 @@ public class AlfanumericoRandomico
                                                       //fondamentali append() e insert().
                                                       //append() "appende" semplicemente una variabile inserita
                                                       //(che può essere di qualsiasi tipo di variabile)
-                                                      //alla fine del costruttore. Per esempio z.append("ciao") aggiunge "ciao" al contenuto di z.
+                                                      //alla fine del costruttore. Per esempio z.append(" ciao") aggiunge " ciao" al contenuto di z.
                                                       //insert() invece inserisce i caratteri in una posizione scelta
                                                       //e si caricano come paramentri un intero (che sarà la posizione nella quale inserire i caratteri)
-                                                      //e la variabile da inserire come carattere. Per esempio 
+                                                      //e la variabile da inserire come carattere. Per esempio z.insert(4, " ciao") inserisce " ciao" nella posizione numero 4
+                                                      //(da tenere a mente che la posizione vale come i vettori e quindi va da 0 in su)
+                                                      //Nel mio github è presente un esempio dell'utilizzo della classe StringBuilder()
+                                                      //(https://github.com/Fraxxs/Scuola-Java/blob/main/EsempioAppend.java)
 
-        for (int i = 0; i < lunghezza; i++)
+        for (int i = 0; i < lunghezza; i++)//Semplice ciclo for che si ripete il numero di volte inserito nel metodo
         {
-            risultato.append(alfanumerico[new Random().nextInt(alfanumerico.length)]);
-        }
-
-        return risultato.toString();
-    }
-}
+            risultato.append(alfanumerico[new Random().nextInt(alfanumerico.length)]);//Qui viene aggiunto all'oggetto "risultato"
+        }                                                                             //un numero o una lettera (create nelle righe 11 e 12).
+                                                                                      //all'interno di append() viene passato alfanumerico[]
+        return risultato.toString();//dato che il metodo è di tipo String             //(le parentesi quadre servono perché stiamo lavorando sul vettore di "alfanumerico")
+                                    //si utilizza un semplice toString()              //all'interno delle parentesi quadre invece troviamo:
+    }                               //per convertire "risultato"                      //[new Random().nextInt(alfanumerico.length)]
+}                                   //(oggetto della classe "StringBuilder()")        //      ^         ^                 ^
+                                    //in String.                                      //      |         |                 |
+                                                                                      //      |         |  questo è il limite che nextInt() può selezionare, .length rappresenta la lunghezza di un vettore, in questo caso la lunghezza del vettore "alfanumerico" (ovvero 61)
+                                                                                      //      |         |  
+                                                                                      //      |     seleziona un numero intero creato nel random
+                                                                                      //      |
+                                                                                      //crea un numero randomico
+                                                                                      //
+                                                                                      //Questo sceglierà quindi un numero che sarà semplicemente la posizione del vettore
+                                                                                      //che verrà appeso a quello che abbiamo già caricato.
+                                                                                      //Per esempio ipotizziamo che il numero scelto casualmente sia 25.
+                                                                                      //La cella numero 25 contiene la lettera "Z" in maiuscolo e quindi al momento riceveremo "Z".
+                                                                                      //Ora il numero scelto casualmente sarà "27".
+                                                                                      //La cella numero 27 contiene la lettera "b" in minuscolo che, venendo appeso a quello che già abbiamo, diventerà "Zb".
+                                                                                      //Questo processo sarà ripetuto per il numero di volte inserito in "lunghezza".
